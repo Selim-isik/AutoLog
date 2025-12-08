88ecb95daca1932fa7897e66d98d535fc768685c
Selim Işık <selimoyunda6@gmail.com>
2025-12-09 00:34:28 +0300

    api.js fixed

--- autolog-frontend/src/api.js
+++ autolog-frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://autolog-backend-api.onrender.com",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {

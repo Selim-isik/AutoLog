816ee650962df04e3a3661ba36b925aebcfc8ba7
Selim Işık <selimoyunda6@gmail.com>
2025-12-09 00:22:59 +0300

    server.js fixed

--- autolog-backend/src/server.js
+++ autolog-backend/src/server.js
@@ -10,14 +10,14 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 export const startServer = () => {
   const app = express();
 
-  const corsOptions = {
-    origin: FRONTEND_URL,
-    credentials: true,
-  };
-
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
-  app.use(cors(corsOptions));
+  app.use(
+    cors({
+      origin: FRONTEND_URL,
+      credentials: true,
+    }),
+  );
   app.use(cookieParser());
 
   app.use(
@@ -36,7 +36,7 @@ export const startServer = () => {
     res.status(404).json({ message: 'Not found' });
   });
 
-  app.use((err, req, res) => {
+  app.use((err, req, res, next) => {
     res
       .status(500)
       .json({ message: 'Something went wrong', error: err.message });
